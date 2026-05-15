aea2f3c9a0b32a2f4e95853509e030db72473749
henriiquejoaao <joaotrindadeguimaraes@gmail.com>
2026-05-15 13:55:23 -0300

    chore: setup express api with typescript

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express"; // express para criar a API
+import cors from "cors"; // permite que o frontend acesse a API em outra porta/domínio
+import dotenv from "dotenv"; // dotenv para ler o .env
+
+// configuração inicial da aplicação
+dotenv.config(); // carrega o arquivo .env
+const app = express(); // cria a aplicação Express (objeto principal do servidor)
+app.use(cors()); // ativa o CORS 
+app.use(express.json()); // permite a API entender o body dos requests (JSON)
+
+// rota de teste para verificar o funcionamento da API
+app.get("/", (request, response) => {
+  return response.json({
+    message: "API Mind Blog rodando!"
+  });
+});
+
+const PORT = process.env.PORT || 3333; // usa a porta do .env ou 3333 como padrão
+
+// inicialização do servidor
+app.listen(PORT, () => { // inicia o servidor e faz ele escutar requisições nessa porta
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
